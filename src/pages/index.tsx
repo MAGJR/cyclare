aac39cfa6508aea6489bd37526f0aa115b06547e
MARCO ANTONIO GONZALES JUNIOR <102200335+MAGJR@users.noreply.github.com>
2023-03-14 20:09:05 -0300

    :tada: Finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { useState, useRef, useEffect } from 'react';
-import { Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading,IconButton, CircularProgress } from '@chakra-ui/react';
 import ReactPlayer, { ReactPlayerProps } from 'react-player';
 import { Lead } from '@/components/Lead';
+import { MdPlayArrow, MdPause } from 'react-icons/md';
 import Head from 'next/head';
 
 import { Root } from '@/components/Root';
@@ -9,8 +10,12 @@ import { MotionBox, animationContainer } from '@/styles/animation';
 
 export default function Home() {
   const [showContent, setShowContent] = useState(false);
-  const playerRef = useRef<ReactPlayer>(null);
+  const [videoProgress, setVideoProgress] = useState(0);
   const [isClient, setIsClient] = useState(false);
+  
+  const playerRef = useRef<ReactPlayer>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setIsClient(true);
@@ -21,6 +26,10 @@ export default function Home() {
       playerRef.current.seekTo(0);
     }
   }, [isClient]);
+
+  function handlePayAndPause () {
+    setIsPlaying((prev) => !prev)
+  }
 
   return (
     <Flex direction="column" align="center" justify="center">
@@ -42,25 +51,58 @@ export default function Home() {
       <MotionBox w={['100%', '70%']} h={['50vh', '70vh']}>
         {isClient && (
           <ReactPlayer
-            url="teste"
+            url="https://d2a7jgldn44rxi.cloudfront.net/VSL%20FINALIZADA.mp4"
             width="100%"
             height="100%"
-            controls
-            onProgress={({ playedSeconds }: ReactPlayerProps) => {
+            controls={false}
+            controlsList="nodownload"
+            playing={isPlaying}
+            ref={playerRef}
+            onProgress={({ playedSeconds, played }: ReactPlayerProps) => {
               const threshold = 10;
-
               if (playedSeconds > threshold) {
                 setShowContent(true);
-
-                
               }
+              setVideoProgress(played);
             }}
-            ref={playerRef}
           />
         )}
+        {isLoading && (
+          <CircularProgress 
+          isIndeterminate
+            size="10%"
+            thickness={0.2}
+            color="gray.500"
+            position="absolute"
+            left="50%"
+            top="50%"
+            transform="translate(-50%, -50%)"
+          />
+        )}
+        <IconButton
+        bgColor="blue.400"
+        _hover={{
+          bgColor: 'blue.500',
+        }}
+          aria-label="play/pause"
+          size="lg"
+          fontSize="40px"
+          variant="ghost"
+          icon={isPlaying ? <MdPause /> : <MdPlayArrow />}
+          position="relative"
+          left="50%"
+          top="-50%"
+          transform="translate(-50%, -50%)"
+          onClick={handlePayAndPause}
+        />
       </MotionBox>
-
+      <div>
+        Progresso do vídeo: {videoProgress * 100}%
+      </div>
       {isClient && showContent && <Lead />}
+
+     
+
       <Root />
     </Flex>
   );
