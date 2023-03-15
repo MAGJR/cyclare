5bf321f9a06b39a85890eb4f29504e033beb5c2c
MARCO ANTONIO GONZALES JUNIOR <102200335+MAGJR@users.noreply.github.com>
2023-03-15 14:56:01 -0300

    :tada: Finished

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { Header } from '@/components/Header'
 
-export default class MyDocument extends Document {
-  render() {
+export default function Document() {
+  
     return (
       <Html>
         <Head>
@@ -27,4 +27,4 @@ export default class MyDocument extends Document {
       </Html>
     )
   }
-}
+
