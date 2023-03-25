22bc122c32bbebb4010510d39afec285c6796540
MARCO ANTONIO GONZALES JUNIOR <102200335+MAGJR@users.noreply.github.com>
2023-03-25 09:48:26 -0300

    :octocat:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import ReactGA from 'react-ga4';
 
+if (typeof window !== 'undefined') {
+  window.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+  })
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   ReactGA.initialize("G-9MVHYB9S60");
