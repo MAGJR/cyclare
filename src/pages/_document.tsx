24c0ade648a6e4d47a3225da8664ffdc19ba0d26
MARCO ANTONIO GONZALES JUNIOR <102200335+MAGJR@users.noreply.github.com>
2023-04-25 19:19:06 -0300

    :rocket:

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,17 +27,7 @@ export default function Document() {
             }}
           />
           
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_GA4}`}></script>
-          <script async dangerouslySetInnerHTML={{
-          __html:`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.GOOGLE_GA4}');
-`
-}}>
-  
-</script>
+         
         </Head>
         <body>
           <Main />
