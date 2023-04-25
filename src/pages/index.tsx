9b30bcc21cefdb97aa06558af997fd47bbdf9eb2
MARCO ANTONIO GONZALES JUNIOR <102200335+MAGJR@users.noreply.github.com>
2023-04-25 19:11:13 -0300

    :rocket:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { Lead } from '@/components/Lead';
 import { Foot } from '@/components/Foot';
 import { MotionBox } from '@/styles/animation';
-import { Box, Flex, Heading, useCallbackRef } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, useCallbackRef } from '@chakra-ui/react';
 
 import { useState, useEffect } from 'react';
 
@@ -41,7 +41,7 @@ export default function HomePage() {
   <Box 
   id="vid_6443c396829ae2000a291b4e" w={["100%","70%"]} h={["50vh", "70vh"]}
   >
-  <img id="thumb_6443c396829ae2000a291b4e" src="https://images.converteai.net/9c1356bc-b9b5-40dd-8e6e-e356cb7978c3/players/6443c396829ae2000a291b4e/thumbnail.jpg" alt="thumbnail"
+  <img alt="thumbnail" id="thumb_6443c396829ae2000a291b4e" src="https://images.converteai.net/9c1356bc-b9b5-40dd-8e6e-e356cb7978c3/players/6443c396829ae2000a291b4e/thumbnail.jpg" 
   style={{position:"absolute",top:"0",left:"0",width:"100%",height:"100%",objectFit:"cover",display:"block"}}/>
   <Box id="backdrop_643860100718a40009eca510" style={{position:"absolute",top:"0",width:"100%",height:"100%",backdropFilter:"blur(5px)"}}></Box>
   </Box>
